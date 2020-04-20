4a2b438562f46cbcff1641ca64ed495e0e9ece58
LongYinan <longyinan.brooklyn@bytedance.com>
2020-04-20 14:39:12 +0800

    feat: replace to crc32fast

--- packages/crc32/build.rs
+++ packages/crc32/build.rs
@@ -1,5 +1,9 @@
 extern crate napi_build;
 
+use std::env;
+
 fn main() {
-  napi_build::setup();
+    napi_build::setup();
+
+    env::set_var("RUSTFLAGS", "-C target-feature=+sse4.2");
 }
--- packages/crc32/src/lib.rs
+++ packages/crc32/src/lib.rs
@@ -1,31 +1,33 @@
 #[macro_use]
 extern crate napi_rs as napi;
 
-use crc32c;
+use crc32fast::Hasher;
 use napi::{Any, Buffer, Env, Error, Number, Object, Result, Status, Value};
 
 register_module!(test_module, init);
 
 fn init<'env>(
-  env: &'env Env,
-  exports: &'env mut Value<'env, Object>,
+    env: &'env Env,
+    exports: &'env mut Value<'env, Object>,
 ) -> Result<Option<Value<'env, Object>>> {
-  exports.set_named_property(
-    "calculate",
-    env.create_function("calculate", callback!(calculate))?,
-  )?;
-  Ok(None)
+    exports.set_named_property(
+        "calculate",
+        env.create_function("calculate", callback!(calculate))?,
+    )?;
+    Ok(None)
 }
 
 fn calculate<'a>(
-  env: &'a Env,
-  _this: Value<'a, Any>,
-  args: &[Value<'a, Any>],
+    env: &'a Env,
+    _this: Value<'a, Any>,
+    args: &[Value<'a, Any>],
 ) -> Result<Option<Value<'a, Number>>> {
-  let input_data = args
-    .get(0)
-    .map(|v| Value::<Buffer>::from_raw(env, v.into_raw()))
-    .ok_or(Error::new(Status::InvalidArg))??;
-  let output = env.create_uint32(crc32c::crc32c(&input_data))?;
-  Ok(Some(output))
+    let input_data = args
+        .get(0)
+        .map(|v| Value::<Buffer>::from_raw(env, v.into_raw()))
+        .ok_or(Error::new(Status::InvalidArg))??;
+    let mut hasher = Hasher::new();
+    hasher.update(&input_data);
+    let output = env.create_uint32(hasher.finalize())?;
+    Ok(Some(output))
 }
