967074eff26a45cae1424700ca02dbc42a5b85a3
LongYinan <lynweklm@gmail.com>
2020-05-06 11:01:21 +0800

    feat(crc32): input now accept string

--- packages/crc32/index.js
+++ packages/crc32/index.js
@@ -1,3 +1,14 @@
 const { locateBinding } = require('@node-rs/helper')
 
-module.exports = require(locateBinding(__dirname, 'crc32'))
+const binding = require(locateBinding(__dirname, 'crc32'))
+
+module.exports = {
+  crc32: function crc32(input, crc) {
+    const _input = Buffer.isBuffer(input) ? input : Buffer.from(input)
+    return binding.crc32(_input, crc)
+  },
+  crc32c: function crc32c(input, crc) {
+    const _input = Buffer.isBuffer(input) ? input : Buffer.from(input)
+    return binding.crc32c(_input, crc)
+  },
+}
