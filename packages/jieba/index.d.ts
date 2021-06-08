c434bfffc904d3707b8fbc93befe67a6fc828cd7
LongYinan <lynweklm@gmail.com>
2021-06-08 23:11:59 +0800

    feat(jieba): implement loadTFIDFDict

--- packages/jieba/index.d.ts
+++ packages/jieba/index.d.ts
@@ -1,5 +1,7 @@
 export function load(): void
 export function loadDict(dict: Buffer): void
+// If you want load dict and TDIDF dict, call `loadDict` before `loadTFIDFDict`
+export function loadTFIDFDict(dict: Buffer): void
 export function cut(sentence: string | Buffer, hmm?: boolean): string[]
 export function cutAll(sentence: string | Buffer): string[]
 
