d91293a675042298b0c5d84e396eadc63916b53a
LongYinan <lynweklm@gmail.com>
2021-10-22 13:07:43 +0800

    feat(xxhash): implement xxh3

--- packages/xxhash/index.d.ts
+++ packages/xxhash/index.d.ts
@@ -14,10 +14,29 @@ export class Xxh32 {
   constructor(seed?: number)
   update(input: BufferLike): this
   digest(): number
+  reset(): void
 }
 
 export class Xxh64 {
   constructor(seed?: BigInt)
   update(input: BufferLike): this
   digest(): BigInt
+  reset(): void
+}
+
+export class Xxh3 {
+  static withSeed(seed?: BigInt): Xxh3
+  static withSecret(secret: BufferLike): Xxh3
+  private constructor() {}
+  update(input: BufferLike): this
+  digest(): BigInt
+  reset(): void
+}
+
+export const xxh3: {
+  xxh64: (input: BufferLike, seed?: BigInt) => BigInt
+  xxh64WithSecret: (input: BufferLike, secret: BufferLike) => BigInt
+  xxh128: (input: BufferLike, seed?: BigInt) => BigInt
+  xxh128WithSecret: (input: BufferLike, secret: BufferLike) => BigInt
+  Xxh3: typeof Xxh3
 }
