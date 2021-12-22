2ab78d9c797a6a133e4c339b39db6572f1d5c8ba
LongYinan <lynweklm@gmail.com>
2021-12-23 00:14:13 +0800

    chore: upgrade rust dependencies

--- crates/alloc/src/lib.rs
+++ crates/alloc/src/lib.rs
@@ -1,6 +1,5 @@
 #[cfg(all(
   not(debug_assertions),
-  not(all(target_os = "windows", target_arch = "aarch64")),
   not(all(target_os = "linux", target_arch = "aarch64", target_env = "musl")),
 ))]
 #[global_allocator]
