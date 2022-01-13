fec1f9bbc6ca24ec90506e926756bd03ee7ad66f
LongYinan <lynweklm@gmail.com>
2022-01-13 20:32:38 +0800

    feat(*): compatible with GLIBC 2.17 (#604)

---
+++ ava.config.js
@@ -0,0 +1,18 @@
+const USE_TS_NODE = (function () {
+  try {
+    require('@swc-node/core')
+    return false
+  } catch (e) {
+    return true
+  }
+})()
+
+module.exports = {
+  extensions: ['ts'],
+  workerThreads: false,
+  require: [USE_TS_NODE ? 'ts-node/register/transpile-only' : '@swc-node/register'],
+  files: ['packages/**/*.spec.ts'],
+  environmentVariables: {
+    TS_NODE_PROJECT: './tsconfig.test.json',
+  },
+}
