b29c93182f55a8d8b0e5c98a368fd1d0328bda43
LongYinan <lynweklm@gmail.com>
2022-03-25 15:24:07 +0800

    chore: disable mimalloc

--- crates/alloc/src/lib.rs
+++ crates/alloc/src/lib.rs
@@ -1,3 +1,6 @@
-#[cfg(all(not(target_env = "musl"), target_arch = "x86_64"))]
+#[cfg(all(
+  all(not(target_env = "musl"), target_arch = "x86_64"),
+  debug_assertions
+))]
 #[global_allocator]
 static GLOBAL: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
