e690b0ff5f26f0672f5a6c2e21e11562737ded36
LongYinan <lynweklm@gmail.com>
2022-05-07 11:53:02 +0800

    Disable worker_thread on centos:7

--- ava.config.js
+++ ava.config.js
@@ -1,24 +1,7 @@
-const USE_TS_NODE = (function () {
-  try {
-    require('@swc-node/core')
-    return false
-  } catch (e) {
-    return true
-  }
-})()
-
 module.exports = {
   extensions: ['ts'],
   workerThreads: true,
-  require: [
-    USE_TS_NODE ? 'ts-node/register/transpile-only' : '@swc-node/register',
-    '@node-rs/argon2',
-    '@node-rs/bcrypt',
-    '@node-rs/crc32',
-    '@node-rs/deno-lint',
-    '@node-rs/jieba',
-    '@node-rs/xxhash',
-  ],
+  require: ['@swc-node/register'],
   files: ['packages/**/*.spec.ts'],
   timeout: '3m',
   environmentVariables: {
