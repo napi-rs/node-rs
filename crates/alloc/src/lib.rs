2188fc91fe940f3e618b87bd61e359aab7abd5ed
LongYinan <lynweklm@gmail.com>
2022-05-07 11:14:59 +0800

    Jemallocator on linux gnu

--- crates/alloc/src/lib.rs
+++ crates/alloc/src/lib.rs
@@ -1,3 +1,11 @@
-#[cfg(all(not(target_env = "musl"), not(target_arch = "aarch64")))]
+#[cfg(not(target_os = "linux"))]
 #[global_allocator]
 static GLOBAL: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
+
+#[cfg(all(
+  target_os = "linux",
+  target_env = "gnu",
+  all(target_arch = "x86_64", target_arch = "aarch64")
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
